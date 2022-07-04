a50e1ffb4bfc56546162edae28beae647e1d8d29
dianaklr <diana.klr148@gmail.com>
2022-07-04 17:31:34 +0300

    hw 2

--- src/components/character.js
+++ src/components/character.js
@@ -1,6 +1,14 @@
 import './character.css'
 
-function Character(){
+function Character(prors){
+    let {name, desc, img} = prors;
 
+    return(
+        <div>
+            <h2>{name}</h2>
+            <p className={'desc'}>{desc}</p>
+            <img src={img}/>
+        </div>
+    )
 }
 export default Character;

8c3bd7c8231ac92f3908048c356e9d35471283cd
dianaklr <diana.klr148@gmail.com>
2022-07-04 14:34:38 +0300

    cw - lesson 1

---
+++ src/components/character.js
@@ -0,0 +1,10 @@
+function Character(){
+    return(
+        <div>
+            <h2>Bart</h2>
+            <img src={"https://upload.wikimedia.org/wikipedia/ru/thumb/2/29/Bart_Simpson.gif/180px-Bart_Simpson.gif"}/>
+        </div>
+    )
+}
+
+export default Character;

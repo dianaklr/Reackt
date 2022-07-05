a6da0a72301ff81f0b21f88dee89807a554ab89c
dianaklr <diana.klr148@gmail.com>
2022-07-05 11:24:59 +0300

    hw 1 (part 2)

--- src/App.js
+++ src/App.js
@@ -5,9 +5,66 @@ import Character from "./components/character";
 function App() {
   return (
       <div>
-        <Character/>
 
+          <div className={`head`}>
+              <h1>The Rick and Morty API</h1>
+          </div>
+          <div className={`body`}>
+        <Character
+            id={157}
+            name={"Hole in the Wall Where the Men Can See it All"}
+            status={"unknown"}
+            species={"unknown"}
+            gender={"Genderless"}
+            image = {'https://rickandmortyapi.com/api/character/avatar/157.jpeg'}
+        />
 
+          <Character
+              id={411}
+              name={"Alien Mexican Armada"}
+              status={"unknown"}
+              species={"Alien"}
+              gender={"Male"}
+              image = {'https://rickandmortyapi.com/api/character/avatar/411.jpeg'}
+          />
+
+          <Character
+              id={447}
+              name={"Anchor Gea"}
+              status={"Alive"}
+              species={"Alien"}
+              gender={"Male"}
+              image = {'https://rickandmortyapi.com/api/character/avatar/447.jpeg'}
+          />
+
+          <Character
+              id={464}
+              name={"SEAL Team Rick"}
+              status={"Dead"}
+              species={"Human"}
+              gender={"Male"}
+              image = {'https://rickandmortyapi.com/api/character/avatar/464.jpeg'}
+          />
+
+          <Character
+              id={691}
+              name={"Scarecrow Beth"}
+              status={"Dead"}
+              species={"Robot"}
+              gender={"Female"}
+              image = {'https://rickandmortyapi.com/api/character/avatar/691.jpeg'}
+          />
+
+          <Character
+              id={691}
+              name={"Amazing Johnathan"}
+              status={"Alive"}
+              species={"Human"}
+              gender={"Male"}
+              image = {'https://rickandmortyapi.com/api/character/avatar/734.jpeg'}
+          />
+
+          </div>
       </div>
   );
 }
@@ -15,11 +72,10 @@ export default App;
 
 
 
+/*
+Створити компонент який описує персонажа (властивості id,name,status,species,gender,image) з цього апі
+https://rickandmortyapi.com/
+https://rickandmortyapi.com/api/character
+Створити 6 персонажів
 
-
-// <div>
-//     <h2>Bart</h2>
-//     <img src={"https://upload.wikimedia.org/wikipedia/ru/thumb/2/29/Bart_Simpson.gif/180px-Bart_Simpson.gif"}/>
-//
-//
-// </div>
+ */
--- src/components/character.js
+++ src/components/character.js
@@ -1,6 +1,21 @@
 import './character.css'
 
-function Character(){
+function Character(prom){
+    let {id,name,status,species,gender,image} = prom
 
+    return(
+        <div className={`person`}>
+            <div>
+            <img alt={name} src={image}/>
+            </div>
+            <div>
+            <p>id: {id}</p>
+            <h2>{name}</h2>
+            <p>status: {status}</p>
+            <p>species: {species}</p>
+            <p>gender: {gender}</p>
+            </div>
+        </div>
+    )
 }
 export default Character;

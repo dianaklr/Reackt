131003e14ce66a1a878d496954e5b7d63033d3e8
dianaklr <diana.klr148@gmail.com>
2022-07-24 14:39:35 +0300

    add pr(base form options)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import { useForm } from 'react-hook-form';
 
 export  default function App() {
-    let {register, handleSubmit} = useForm();
+    let {register, handleSubmit, watch, formState:{errors}} = useForm({defaultValues: {title: 'your name'}});
 
     const onSubmit = (data) =>{
         console.log(data)
     }
 
-  return (
+    console.log(watch('title'));
+
+    return (
       <div>add
           <form onSubmit={handleSubmit(onSubmit)}>
                 {/*спед регістра - автоматично зробить нейм з тайтлом - автоматизація*/}
                 <input {...register("title")}/>
+                {errors.title && <span>ERROR</span>}
                 <input {...register("body")}/>
                 <input type="submit" value={'save post'}/>
+
+              <select {...register('userId')}>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5"}>5</option>
+                  <option value="6">6</option>
+                  <option value="7">7</option>
+                  <option value="8">8</option>
+                  <option value="9">9</option>
+                  <option value="10">10</option>
+
+
+
+
+              </select>
           </form>
       </div>
   );
